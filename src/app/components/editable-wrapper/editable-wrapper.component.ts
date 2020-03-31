25fd24750a22b6021d5423d5ce57c5e9e54c1b72
Chui Hin Wah (Dickson) <dicksonZero@gmail.com>
2020-03-31 18:42:54 +0800

    Step title Styling

--- src/app/components/editable-wrapper/editable-wrapper.component.ts
+++ src/app/components/editable-wrapper/editable-wrapper.component.ts
@@ -12,6 +12,7 @@ export class EditableWrapperComponent implements OnInit {
 
   isEdit = false;
   isLabelOver = false;
+  @Input() wide = false;
   @Input() value = '';
   @Output() valueChange = new EventEmitter<string>();
 
