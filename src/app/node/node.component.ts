646feacf0e764beeb20d54a7c3e83ad2c27bcda0
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2020-06-15 23:20:04 +0800

    Render Node; Add Regular icons; Add css clip-path

--- src/app/node/node.component.ts
+++ src/app/node/node.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { TABLET_PORTRAIT } from 'src/media';
 import { INodeInstance } from '../s/new-model/appDefinitions';
+import { operators } from '../s/operators';
+import { faBolt, faEdit, faCaretSquareLeft, faCaretSquareRight, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faCaretSquareLeft as faCaretSquareLeftR, faCaretSquareRight as faCaretSquareRightR } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-node',
@@ -9,10 +12,22 @@ import { INodeInstance } from '../s/new-model/appDefinitions';
 })
 export class NodeComponent implements OnInit {
 
+  public faBolt = faBolt;
+  public faEdit = faEdit;
+  public faChevronLeft = faChevronLeft;
+  public faChevronRight = faChevronRight;
+
+  public faCaretSquareLeft = faCaretSquareLeft;
+  public faCaretSquareRight = faCaretSquareRight;
+  public faCaretSquareLeftR = faCaretSquareLeftR;
+  public faCaretSquareRightR = faCaretSquareRightR;
+
   @Input() stepID: number;
   @Input() nodeIndex: number;
   @Input() nodeInstance: INodeInstance;
   isMobile = false;
+
+  operatorList = Object.keys(operators);
 
   ngOnInit() {
     this.updateDeviceWidth();
